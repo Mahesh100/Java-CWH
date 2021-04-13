fbc16347f2fbcf95b025b1ce36c013f29d55594c
Mahesh Kakde <maheshkakde100@gmail.com>
2021-04-13 22:02:23 +0530

    Increment and Decrement Operators

---
+++ CWH_10_resulting_data_type.java
@@ -0,0 +1,15 @@
+public class CWH_10_resulting_data_type {
+
+    public static void main(String[] args) {
+        
+        byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y + z;
+        float b = 6.45f + x;
+        System.out.println(b);
+        
+    }
+    
+}
+ 
